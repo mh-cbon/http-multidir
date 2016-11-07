b4a1f50cff2477d93ed80ae0ae8756deec1d6e0b
mh-cbon <mh-cbon@users.noreply.github.com>
2016-11-07 21:01:44 +0100

    example

--- example_main_test.go
+++ example_main_test.go
@@ -5,8 +5,10 @@ import (
   "github.com/mh-cbon/http-multidir"
 )
 
-func Example() {
+// Example_main demonstrates usage of multidir package.
+func Example_main() {
   dirs := []string{"static/", "other_static/", "upload/",}
   fileServer := http.FileServer(httpmultidir.Multidir(dirs))
   http.Handle("/prefixed/", http.StripPrefix("/prefixed/", fileServer))
+  http.ListenAndServe(":8080", nil)
 }
