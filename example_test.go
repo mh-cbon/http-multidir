d93231e68b2705f9452dc10be0c157a254e2e252
mh-cbon <mh-cbon@users.noreply.github.com>
2016-11-07 20:51:39 +0100

    example

--- example_test.go
+++ example_test.go
@@ -5,7 +5,7 @@ import (
   "github.com/mh-cbon/http-multidir"
 )
 
-func Example_main() {
+func Example() {
   dirs := []string{"static/", "other_static/", "upload/",}
   fileServer := http.FileServer(httpmultidir.Multidir(dirs))
   http.Handle("/prefixed/", http.StripPrefix("/prefixed/", fileServer))
